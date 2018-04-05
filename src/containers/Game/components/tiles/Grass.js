2ab851c436e47ae16f5b0ee8d041876ea81f527d
Cristian Lara <cristianlara@stanford.edu>
2018-04-05 16:32:45 -0700

    clean up grass tile to match others

--- src/containers/Game/components/tiles/Grass.js
+++ src/containers/Game/components/tiles/Grass.js
@@ -29,54 +29,35 @@ class Grass extends Tile {
     super(props);
 
     this.state = {
-      directory: 'grass',
-      type: '',
-      grassSource: undefined,
-      grassSource3D: undefined,
+      type: 'grass',
+      style: 'grass1',
       visible: false,
     }
 
     this.depth = true;
-    this.tileTypes = [
+    this.styles = [
       'grass1', 'grass2', 'grass3', 'grass4', 'grass5'
     ];
 
     this.setDepth = this.setDepth.bind(this);
   }
 
-  toggleHD(hd) {
-    var newDirectory = '';
-    if (hd) {
-      newDirectory = 'grass_hd'
-    } else {
-      newDirectory = 'grass'
-    }
-    this.setState({ grassSource: require(`../../../../graphics/tiles/${newDirectory}/${this.type}.png`)});
-  }
-
   componentWillMount() {
+    super.componentWillMount();
+
     var num = Math.random();
-    if (num < 0.4) this.type = this.tileTypes[0];
-    else if(num < 0.8) this.type = this.tileTypes[1];
-    else if(num < 0.88) this.type = this.tileTypes[2];
-    else if(num < 0.96) this.type = this.tileTypes[3];
-    else this.type = this.tileTypes[4];
+    var chosenStyle = '';
+    if (num < 0.4) chosenStyle = this.styles[0];
+    else if(num < 0.8) chosenStyle = this.styles[1];
+    else if(num < 0.88) chosenStyle = this.styles[2];
+    else if(num < 0.96) chosenStyle = this.styles[3];
+    else chosenStyle = this.styles[4];
 
-    if (this.props.hd) this.state.directory += '_hd';
-
-    const grassSource = require(
-      `../../../../graphics/tiles/${this.state.directory}/${this.type}.png`
-    );
-    let grassSource3D = undefined;
-    if (this.type === 'grass_tall') {
-      grassSource3D = require(
-        `../../../../graphics/tiles/${this.state.directory}/${this.type}_3d.png`
-      );
-    } else if (this.type === 'grass_tree') {
+    if (chosenStyle === 'grass_tree') {
       this.walkable = false;
     }
 
-    this.setState({ type: this.type, grassSource: grassSource, grassSource3D: grassSource3D });
+    this.setState({ style: chosenStyle });
   }
 
   setDepth(depth) {
@@ -84,21 +65,22 @@ class Grass extends Tile {
   }
 
   render() {
-    const { type, grassSource, grassSource3D, visible } = this.state;
+    const { type, style, visible } = this.state;
+    const grassSource = require(`../../../../graphics/tiles/${type}/${style}.png`);
 
-    let tile = <StyledGrass src={grassSource}/>;
     if (type === 'grass_tall') {
-      tile = (
+      const grassSource3D = require(`../../../../graphics/tiles/${type}/${style}_3d.png`);
+      return (
         <Grass3DContainer>
           <StyledGrass src={grassSource}/>
           <Grass3D visible={visible} src={grassSource3D}/>
         </Grass3DContainer>
       );
     }
-
+    
     return (
-      tile
-    );
+      <StyledGrass src={grassSource}/>
+    )
   }
 
 }
